6074c68fcba6ccb05026cd62df7993cec7fb337f
Bordinassi <123772388+jusenai2023@users.noreply.github.com>
2023-08-02 13:28:20 -0300

    Add files via upload

---
+++ dogs.js
@@ -0,0 +1,53 @@
+// Questão 1//
+let myAge = 16;
+
+/* Nesta linha de código, criamos uma variável chamada myAge e atribuímos a ela o valor 28, 
+que é a minha idade atual em anos. O comentário acima da linha explica que esta variável 
+é usada para armazenar a minha idade em anos humanos. O valor 28 é apenas um exemplo; 
+você pode substituí-lo pelo valor correspondente à sua própria idade.*/
+
+// Questão 2//
+earlyYears *= 10.5;
+
+// Questão 3//
+let earlyYears = 2;
+
+/*esta linha de código, criamos uma variável chamada earlyYears e atribuímos a ela o valor 2. 
+O comentário acima da linha explica que esses são os dois primeiros anos da vida de um cão. 
+Em muitos sistemas de cálculo da idade de cães, acredita-se que esses primeiros anos correspondem
+ a uma fase de rápido desenvolvimento, portanto, contam mais do que os anos subsequentes. 
+Essa fase é geralmente chamada de "anos de cachorro" ou "anos iniciais".*/
+
+// Questão 4//
+earlyYears = - 2;
+
+// Questão 5//
+earlyYears *= 4;
+
+// Questão 06//
+
+console.log(earlyYears); 
+console.log(laterYears); 
+
+// Questão 07//
+
+let myAgeInDogYears = earlyYears + laterYears; 
+// Representa minha idade em anos de cão.
+
+// Questão 08//
+
+let myName = "Giovanna Oliveira".toLowerCase(); 
+// Meu nome em letras minúsculas.
+
+// Questão 09//
+
+console.log(`Meu nome é ${myName}. Tenho ${myAge} anos em anos humanos, que são ${myAgeInDogYears} anos em anos de cão.`);
+
+//Desafio Extra//
+const myAge1 = 17;
+let earlyYears1 = 2 * 10.5;
+let laterYears1 = (myAge1 - 2) * 4;
+const myAgeInDogYears1 = earlyYears1 + laterYears1;
+const myName1 = "Giovanna Oliveira".toLowerCase();
+
+console.log(`Meu nome é ${myName1}. Tenho ${myAge1} anos em anos humanos, que são ${myAgeInDogYears1} anos em anos de cão.`);
